3bb5d2c27aba4e0cfba4302fe8245d6faf72e77a
Abhishek kumar verma <145225500+abhishek6393@users.noreply.github.com>
2023-09-26 01:22:01 +0530

    Update PHONE_KEYBOARD.java

--- PHONE_KEYBOARD.java
+++ PHONE_KEYBOARD.java
@@ -1,24 +1,35 @@
 package home_work;
+import java.util.*;
 
 public class PHONE_KEYBOARD {
 	static String[] map= {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s="23";
-		print(s,"");
+		List<String>ll=new ArrayList<>();
+		
+		String s="";
+		if(s.length()==0) {
+			System.out.println(ll);
+			return;
+		}
+		print(s,"",ll);
+		
 
 	}
-   public static void print(String s,String ans) {
+   public static void print(String s,String ans,List<String>ll) {
+	   
+	   
 	   if(s.length()==0) {
-		   System.out.println(ans);
+//		   System.out.println(ans);
+		   ll.add(ans);
 		   return;
 	   }
 	   
 	   char ch=s.charAt(0);
 	   String press=map[ch-'0'];
 	   for(int i=0;i<press.length();i++) {
-		   print(s.substring(1),ans+press.charAt(i));
+		   print(s.substring(1),ans+press.charAt(i),ll);
 	   }
 	   
    }
